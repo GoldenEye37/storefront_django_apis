442fcd735927b46b452d447318c265d7e299cb7b
Leon <dhimbaleon356@gmail.com>
2022-08-12 14:39:56 +0200

    retrive order

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from ast import Return
 from itertools import count, product
 from math import fabs
+from multiprocessing import context
 from django.db.models import Count
 from django.http import HttpResponse
 from requests import request
@@ -125,14 +126,19 @@ class OrderViewSet(ModelViewSet):
     
     permission_classes = [IsAuthenticated]
 
+    def create(self, request, *args, **kwargs):
+        serializer = CreateOrderSerializer(
+            data=request.data,
+            context =  {'user_id': self.request.user.id})
+        serializer.is_valid(raise_exception=True)
+        order = serializer.save()
+        serializer = OrderSerializer(order)
+        return Response(serializer.data) 
+
     def get_serializer_class(self):
         if self.request.method=='POST':
             return CreateOrderSerializer
         return OrderSerializer
-
-    def get_serializer_context(self):
-
-        return {'user_id': self.request.user.id}
 
     def get_queryset(self):
         user = self.request.user
