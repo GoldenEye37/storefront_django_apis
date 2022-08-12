2ea7b9a339116956c78c1957047446e71e536f72
Leon <dhimbaleon356@gmail.com>
2022-08-12 15:23:47 +0200

    order permissions(Updated)

--- store/views.py
+++ store/views.py
@@ -123,8 +123,12 @@ class CustomerViewSet(ModelViewSet):
 
 class OrderViewSet(ModelViewSet):
 
-    
-    permission_classes = [IsAuthenticated]
+    http_method_names = [ 'get', 'patch', 'delete', 'heads', 'options']
+
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE']:
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
 
     def create(self, request, *args, **kwargs):
         serializer = CreateOrderSerializer(
