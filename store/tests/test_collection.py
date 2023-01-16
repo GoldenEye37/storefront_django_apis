b2a51cc5c9e34127b4bb2370e968faecb5db7425
LDhimba <Ldhimba@bancabc.co.zw>
2023-01-16 15:06:57 +0200

    using multiple asserts in testing

--- store/tests/test_collection.py
+++ store/tests/test_collection.py
@@ -2,6 +2,9 @@ import pytest
 from rest_framework import status
 from rest_framework.test import APIClient
 
+from core.models import User
+
+
 @pytest.mark.django_db
 class TestCreateCollection:
 
@@ -24,3 +27,21 @@ class TestCreateCollection:
 
         # Assert
         assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_data_is_invalid_returns_400(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post('/store/collections/', {'title': ''})
+
+        # Assert
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['title'] is not None
+
+    def test_if_data_is_valid_returns_201(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post('/store/collections/', {'title': 'a'})
+
+        # Assert
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data['id'] > 0
