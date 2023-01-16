01eba761eb120237c6f64b578c563b11a97207b2
LDhimba <Ldhimba@bancabc.co.zw>
2023-01-16 14:53:46 +0200

    test user is not admin

--- store/tests/test_collection.py
+++ store/tests/test_collection.py
@@ -17,3 +17,10 @@ class TestCreateCollection:
         # Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
+    def test_if_user_is_not_admin_403(self):
+        client = APIClient()
+        client.force_authenticate(user={})
+        response = client.post('/store/collections/', {'title': 'a'})
+
+        # Assert
+        assert response.status_code == status.HTTP_403_FORBIDDEN
